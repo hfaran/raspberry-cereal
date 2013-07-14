d2fd055971b318e5961eb4020407ad1e5193a3a3
Core2uu <hamzafaran@hotmail.com>
2013-07-13 21:11:50 -0700

    Fixed typo, formatting

--- raspberry_cereal/__init__.py
+++ raspberry_cereal/__init__.py
@@ -3,8 +3,7 @@ from ConfigParser import ConfigParser
 import time
 
 from raspberry_cereal.custom_device import CustomDevice
-from raspberry_cereal.sr_74hc165n import gpio_setup, \
-    read_shift_reg
+from raspberry_cereal.sr_74hc165n import gpio_setup, read_shift_reg
 from raspberry_cereal.constants import CONFIG_PATH, POLL_TIME
 
 def main():
@@ -21,7 +20,7 @@ def main():
     # Setup GPIO, create inverse pin/key dict
     sr_config = gpio_setup()
     bit2key_map = {config.get('KEY2BIT_MAP', option):option for option in config.options('KEY2BIT_MAP')}
-    print "Finished setup in {}. Polling every {} ms.".format(
+    print "Finished setup in {} seconds. Polling every {} ms.".format(
                 time.time() - start,
                 POLL_TIME*1000
         )
@@ -30,5 +29,9 @@ def main():
         serial_input = read_shift_reg(sr_config)
         for bit in enumerate(serial_input):
             if bit[1]:
-                device.emit_click(eval("uinput.{}".format(bit2key_map[int(bit[0])])))
+                device.emit_click(eval("uinput.{}".format(
+                            bit2key_map[str(bit[0])].upper()
+                        )
+                    )
+                )
         time.sleep(POLL_TIME)
