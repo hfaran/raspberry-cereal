ffcd114c2c99839a7b49429a2879cbe06aaa59ed
Core2uu <hamzafaran@hotmail.com>
2013-07-20 13:27:41 -0700

    * Implement dynamic load calculation * Change implementation of device emits so that it mirrors how keyboard presses work * Update config and validation

--- raspberry_cereal/__init__.py
+++ raspberry_cereal/__init__.py
@@ -9,7 +9,6 @@ import time
 from ConfigParser import ConfigParser
 import argparse
 
-from raspberry_cereal.custom_device import CustomDevice
 from raspberry_cereal.sr_74hc165n import gpio_setup, read_shift_regs
 from raspberry_cereal.constants import CONFIG_PATH, BHZ_PER_CPU_PERCENT
 from raspberry_cereal.validate_cfg import main as validate_config
@@ -39,25 +38,23 @@ def main():
     config.read(CONFIG_PATH)
     #
     ACTIVE_LOW = eval(config.get('RASPBERRY_CEREAL', 'active_low'))
+    # Create device
+    events = []
+    for key in [config.get('BIT2KEY_MAP', num) \
+            for num in config.options('BIT2KEY_MAP') \
+            if config.get('BIT2KEY_MAP', num) != "NONE"]:
+        events.append(eval("uinput.{}".format(key.upper())))
+    device = uinput.Device(events)
+    print "[OK] Configuration options type-validated."
+    # Setup GPIO
+    sr_config = gpio_setup()
     # Set poll time
     if eval(config.get('RASPBERRY_CEREAL', 'autocalculate_poll_time')):
-        poll_time = eval(config.get('RASPBERRY_CEREAL', 'bus_width')) * \
-            eval(config.get('RASPBERRY_CEREAL', 'shift_registers')) / \
-            BHZ_PER_CPU_PERCENT / eval(config.get('RASPBERRY_CEREAL', \
-            'approx_cpu_usage_limit'))
+        poll_time = test(sr_config, device, 10000)
         print "[OK] Poll time calculated: {} ms".format(poll_time*1000)
     else:
         poll_time = float(config.get('RASPBERRY_CEREAL', 'poll_time'))
-    # Create device
-    events = []
-    for key in config.options('KEY2BIT_MAP'):
-        events.append(eval("uinput.{}".format(key.upper())))
-    device = CustomDevice(events)
-    print "[OK] Configuration options type-validated."
-    # Setup GPIO, create inverse pin/key dict
-    sr_config = gpio_setup()
-    bit2key_map = {config.get('KEY2BIT_MAP', option): option
-                   for option in config.options('KEY2BIT_MAP')}
+
     print ("[OK] If you opened {0} with {1}, you may safely hit Ctrl-C and"
            " {0} will continue to run in the background. Remember to kill"
            " the job when you are done. Polling every {2} ms.".format(
@@ -65,23 +62,32 @@ def main():
                "'sudo raspberry-cereal &'",
                int(poll_time*1000)))
 
-    old_input = [int(ACTIVE_LOW) for i in range(eval(config.get( \
-        'RASPBERRY_CEREAL', 'bus_width')) * eval(config.get('RASPBERRY_CEREAL', \
-        'shift_registers')))]
     while(True):
         try:
             serial_input = read_shift_regs(sr_config)
             if args.debug:
                 print serial_input
             else:
-                for bit in enumerate(serial_input):
-                   if bit[1] == int(not ACTIVE_LOW) and old_input[bit[0]] == int(ACTIVE_LOW):
-                       device.emit_click(
-                           eval(
-                               "uinput.{}".format(
-                                   bit2key_map[str(bit[0])].upper())))
+                emit_key(serial_input, config, device)
             time.sleep(poll_time)
-            old_input = serial_input
         except KeyboardInterrupt:
           exit("[OK] raspberry-cereal bids you adieu.")
 
+def test(sr_config, device, RUNS):
+    """Times RUNS runs and returns poll_time based on the specified"""
+    start = time.time()
+    for i in range(RUNS):
+        serial_input = read_shift_regs(sr_config)
+        device.emit(uinput.KEY_UNKNOWN, 0)
+    return (float(RUNS)/(time.time() - start))*(eval(config.get('RASPBERRY_CEREAL', 'approx_cpu_usage_limit'))/100.0)
+
+def emit_key(serial_input, config, device):
+    for bit in enumerate(serial_input):
+        key = config.get('BIT2KEY_MAP', str(bit[0])).upper()
+        if key != "NONE":
+            if bit[1] == int(not ACTIVE_LOW):
+                device.emit(
+                    eval("uinput.{}".format(key)), 1)
+            else:
+                device.emit(
+                    eval("uinput.{}".format(key)), 0)
